924e1d90dbd849a62e54bcef2e50f4da8dbae963
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-05 11:29:11 +0100

    refactor: refactor store code base

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import create from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
 
 import Router from 'next/router';
 
@@ -9,48 +10,60 @@ import { query, collection, getDocs } from 'firebase/firestore';
 import { toast } from 'react-hot-toast';
 
 import { firestoreDb } from 'src/libs';
-import { AppState, formData } from 'src/types';
+import { AppState, Admin } from 'src/types';
 
-export const useStore = create<AppState>((set) => ({
-  crimes: [],
-  isLoadingCrime: false,
-  isLoadingUser: false,
-  user: null,
+const useAppStore = create<AppState>()(
+  devtools(
+    persist(
+      (set) => ({
+        crimes: null,
+        isLoadingCrime: false,
+        isLoadingUser: false,
+        user: null,
 
-  // methods
-  loginAdmin: ({ email, password }: formData) => {
-    const auth = getAuth();
-    set((state) => ({ isLoadingUser: (state.isLoadingUser = true) }));
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        set((state) => ({ user: (state.user = user) }));
-        set((state) => ({ isLoadingUser: (state.isLoadingUser = false) }));
-        localStorage.setItem('users', JSON.stringify(user));
-        console.log(userCredential);
-        Router.push('/dashboard');
-      })
-      .catch((error) => {
-        console.log(error);
-        set((state) => ({ isLoadingUser: (state.isLoadingUser = false) }));
-      });
-  },
-  fetchCrimes: async () => {
-    set((state) => ({ isLoadingCrime: (state.isLoadingCrime = true) }));
-    const crimeQuery = query(collection(firestoreDb, 'crimes'));
-    await getDocs(crimeQuery)
-      .then((snapshot) => {
-        set((state) => ({ crimes: (state.crimes = snapshot.docs) }));
-        set((state) => ({ isLoadingCrime: (state.isLoadingCrime = false) }));
-        localStorage.setItem('crimes', JSON.stringify(snapshot.docs));
-      })
-      .catch((err) => {
-        set((state) => ({ isLoadingCrime: (state.isLoadingCrime = false) }));
-        console.log(err);
-        toast('Error fetching crimes', {
-          position: 'bottom-center',
-          duration: 5000,
-        });
-      });
-  },
-}));
+        // methods
+        loginAdmin: ({ email, password }: Admin) => {
+          const auth = getAuth();
+          set((state) => ({ isLoadingUser: (state.isLoadingUser = true) }));
+          signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+              const user = userCredential.user;
+              set((state) => ({ ...state, user: user }));
+              set((state) => ({ ...state, isLoadingUser: false }));
+              Router.push('/dashboard');
+            })
+            .catch((error) => {
+              console.log(error);
+              set((state) => ({ ...state, isLoadingUser: false }));
+            });
+        },
+        fetchCrimes: async () => {
+          set((state) => ({ ...state, isLoadingCrime: true }));
+          const crimeQuery = query(collection(firestoreDb, 'crimes'));
+          await getDocs(crimeQuery)
+            .then((snapshot) => {
+              set((state) => ({
+                ...state,
+                isLoadingCrime: false,
+              }));
+              set((state) => ({
+                ...state,
+                crimes: snapshot?.docs.map((doc) => {
+                  return { ...doc.data(), id: doc.id };
+                }),
+              }));
+            })
+            .catch((err) => {
+              set((state) => ({ ...state, isLoadingCrime: false }));
+              console.log(err);
+            });
+        },
+      }),
+      {
+        name: 'app-storage',
+      }
+    )
+  )
+);
+
+export { useAppStore };
