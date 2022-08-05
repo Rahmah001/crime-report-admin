4b04cca068a9cce2fa968415fb9cfa87ebcd173e
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-05 11:24:54 +0100

    chore: updated some types objects, crimes uses DocumentData[] instead of QueryDocumentData<Document>[]

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,20 @@
 import { User } from 'firebase/auth';
 import { DocumentData, QueryDocumentSnapshot } from 'firebase/firestore';
 
-export type formData = { email: string; password: string };
+export type Admin = { email: string; password: string };
+
+export type CrimeState = {
+  phoneNumber: string;
+  crime: string;
+  name: string;
+  email: string;
+};
 
 export type AppState = {
   user: User | null;
-  crimes: QueryDocumentSnapshot<DocumentData>[];
+  crimes: DocumentData[] | [] | null;
   isLoadingCrime: boolean | undefined;
   isLoadingUser: boolean | undefined;
   fetchCrimes: () => void;
-  loginAdmin: (formData: formData) => void;
+  loginAdmin: (user: Admin) => void;
 };
