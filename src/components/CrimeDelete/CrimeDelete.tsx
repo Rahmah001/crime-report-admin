d0db54644aee55c9357012c754c5ba21fe2b24a3
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-05 18:41:57 +0100

    chore: CrimeDelete component composition

---
+++ src/components/CrimeDelete/CrimeDelete.tsx
@@ -0,0 +1,49 @@
+import { DeleteIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  IconButton,
+} from '@chakra-ui/react';
+
+import { CrimeProps } from 'src/types';
+
+const CrimeDelete: React.FC<CrimeProps> = ({ crime }) => {
+  const { onOpen, onClose, isOpen } = useDisclosure();
+
+  return (
+    <Box>
+      <IconButton
+        aria-label="delete-button"
+        size="xs"
+        colorScheme="red"
+        variant="ghost"
+        onClick={onOpen}
+        icon={<DeleteIcon />}
+      />
+      <Modal
+        isCentered
+        size="lg"
+        onClose={onClose}
+        isOpen={isOpen}
+        motionPreset="slideInBottom"
+        closeOnOverlayClick={false}
+      >
+        <ModalOverlay backdropFilter={'blur(4px)'} />
+        <ModalContent>
+          <ModalHeader fontSize={'sm'}>
+            Delete {crime?.name} reported crime on {crime?.crime}
+          </ModalHeader>
+          <ModalCloseButton size="sm" />
+        </ModalContent>
+      </Modal>
+    </Box>
+  );
+};
+
+export default CrimeDelete;
