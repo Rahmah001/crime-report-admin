2e7af1193039e058ab041a0fa50e6dff42d88d51
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-05 11:40:48 +0100

    chore: added react-hot-toast component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,18 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import { Toaster } from 'react-hot-toast';
 
-import { useAppStore } from 'src/store';
 import { theme } from 'src/theme';
 
 import '../index.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <Toaster />
+    </>
   );
 };
 
