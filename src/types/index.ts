66fc206cfbf53c60b9cac4d2f4e61fc72caee658
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-05 11:41:45 +0100

    chore: types configs

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,7 @@
 import { User } from 'firebase/auth';
-import { DocumentData, QueryDocumentSnapshot } from 'firebase/firestore';
+import { DocumentData } from 'firebase/firestore';
 
 export type Admin = { email: string; password: string };
-
-export type CrimeState = {
-  phoneNumber: string;
-  crime: string;
-  name: string;
-  email: string;
-};
 
 export type AppState = {
   user: User | null;
