b418029ed1b35300c7557ff376fff5b5b4047774
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-05 18:41:09 +0100

    chore: AttentedToCrime tab component

---
+++ src/components/AttendedToCrimesTab/AttendedToCrimesTab.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+
+import {
+  Center,
+  HStack,
+  Box,
+  Spinner,
+  TableContainer,
+  Text,
+  Table,
+  TableCaption,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+} from '@chakra-ui/react';
+import CrimeActions from 'src/components/CrimeActions/CrimeActions';
+
+import { useAppStore } from 'src/store';
+import { CrimeType } from 'src/types';
+
+const AttendedToCrimeTab = () => {
+  const isLoading = useAppStore((state) => state.isLoadingCrime);
+  const crimes = useAppStore((state) => state.crimes);
+
+  const [crimesState, setCrimesState] = useState<CrimeType>([]);
+  return (
+    <Box>
+      {isLoading && (
+        <Center my={3}>
+          <HStack spacing={3}>
+            <Spinner emptyColor="gray.200" color="purple.500" size={'sm'} />
+            <Text fontSize={'14px'}>Fetching crimes</Text>
+          </HStack>
+        </Center>
+      )}
+      <TableContainer border={'1px solid #EDF2F7'} rounded={'lg'}>
+        <Table variant="simple" size={'sm'}>
+          <TableCaption>CrimeReport. Data</TableCaption>
+          <Thead p={4}>
+            <Tr>
+              <Th>Email Address</Th>
+              <Th>Name</Th>
+              <Th>Phone Number</Th>
+              <Th>Crime</Th>
+              <Th>Actions</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {crimesState?.map((crime) => (
+              <Tr key={crime?.id}>
+                <Td>{crime?.email}</Td>
+                <Td>{crime?.name}</Td>
+                <Td>{crime?.phoneNumber}</Td>
+                <Td>{crime?.crime}</Td>
+                <Td>
+                  <CrimeActions crime={crime} />
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
+
+export default AttendedToCrimeTab;
