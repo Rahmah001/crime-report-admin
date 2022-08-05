7f3ba039afa22f5896fc97fd812c0174f16ca24c
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-05 18:43:56 +0100

    chore: Added two new Type definition, crime and crimeProps

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,12 +2,17 @@ import { User } from 'firebase/auth';
 import { DocumentData } from 'firebase/firestore';
 
 export type Admin = { email: string; password: string };
+export type CrimeType = DocumentData[] | null | [];
 
 export type AppStore = {
   user: User | null;
-  crimes: DocumentData[] | [] | null;
+  crimes: CrimeType;
   isLoadingCrime: boolean | undefined;
   isLoadingUser: boolean | undefined;
   fetchCrimes: () => void;
   loginAdmin: (user: Admin) => void;
+};
+
+export type CrimeProps = {
+  crime: DocumentData;
 };
