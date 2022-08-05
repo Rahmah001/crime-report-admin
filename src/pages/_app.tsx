4dc80db397bd54a2540bcd2a6bb97afd5e922bbe
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-05 11:26:53 +0100

    chore: converted _app.tsx to an arrow function and removed react-hot-toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,21 @@
+import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
 
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { Toaster } from 'react-hot-toast';
 
+import { useAppStore } from 'src/store';
 import { theme } from 'src/theme';
 
 import '../index.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
-      <Toaster />
     </ChakraProvider>
   );
-}
+};
 
 export default MyApp;
