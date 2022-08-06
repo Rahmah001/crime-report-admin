0fe48fac018140862bb4e1a6b6347331d09a6807
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-06 17:53:38 +0100

    chore: handle client only to solve hydration errors

---
+++ src/components/ClientOnly/ClientOnly.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren, useEffect, useState } from 'react';
+
+const ClientOnly = ({ children }: PropsWithChildren) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return <>{children}</>;
+};
+
+export default ClientOnly;
