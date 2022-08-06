1e9763d4618ba5548d54fd70dcf07842a665ed09
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-06 16:00:53 +0100

    refactor: refactor store folder, moved all init states to initialState objects and actions to its own folder

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,103 +1,27 @@
 import create from 'zustand';
 
-import toast from 'react-hot-toast';
-import Router from 'next/router';
-
 import { devtools, persist } from 'zustand/middleware';
 
-import { AuthError, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { AppStore } from 'src/types';
+import { StoreActions } from './actions';
 
-import {
-  query,
-  collection,
-  getDocs,
-  FirestoreError,
-  where,
-} from 'firebase/firestore';
-
-import { firestoreDb } from 'src/libs';
-import { AppStore, Admin } from 'src/types';
+const initialState = {
+  crimes: null,
+  attendedToCrimes: null,
+  nonAttendedToCrimes: null,
+  isLoadingCrime: false,
+  isLoadingUser: false,
+  user: null,
+  isLoadingEdit: false,
+};
 
 const useAppStore = create<AppStore>()(
   devtools(
     persist(
-      (set) => ({
-        crimes: null,
-        attendedToCrimes: null,
-        isLoadingCrime: false,
-        isLoadingUser: false,
-        user: null,
-
+      (_) => ({
+        ...initialState,
         // methods
-        loginAdmin: ({ email, password }: Admin) => {
-          const auth = getAuth();
-          set((state) => ({ ...state, isLoadingUser: true }));
-          signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-              const user = userCredential.user;
-              set((state) => ({ ...state, user: user }));
-              set((state) => ({ ...state, isLoadingUser: false }));
-              toast.success('Login successful', {
-                position: 'bottom-center',
-              });
-              Router.push('/dashboard');
-            })
-            .catch((error: AuthError) => {
-              set((state) => ({ ...state, isLoadingUser: false }));
-              toast.error(`${error.message}`, {
-                position: 'bottom-center',
-              });
-            });
-        },
-        fetchCrimes: async () => {
-          set((state) => ({ ...state, isLoadingCrime: true }));
-          const crimeQuery = query(collection(firestoreDb, 'crimes'));
-          await getDocs(crimeQuery)
-            .then((snapshot) => {
-              set((state) => ({
-                ...state,
-                isLoadingCrime: false,
-              }));
-              set((state) => ({
-                ...state,
-                crimes: snapshot?.docs.map((doc) => {
-                  return { ...doc.data(), id: doc.id };
-                }),
-              }));
-            })
-            .catch((error: FirestoreError) => {
-              set((state) => ({ ...state, isLoadingCrime: false }));
-              toast.error(`${error.message}`, {
-                position: 'bottom-center',
-              });
-            });
-        },
-        fetchCrimesAttendedTo: async () => {
-          set((state) => ({ ...state, isLoadingCrime: true }));
-          const attentedCrimeQuery = query(
-            collection(firestoreDb, 'crimes'),
-            where('attendedTo', '==', true)
-          );
-          await getDocs(attentedCrimeQuery)
-            .then((snapshot) => {
-              set((state) => ({
-                ...state,
-                isLoadingCrime: false,
-              }));
-              set((state) => ({
-                ...state,
-                attendedToCrimes: snapshot?.docs.map((doc) => {
-                  return { ...doc.data(), id: doc.id };
-                }),
-              }));
-            })
-            .catch((error: FirestoreError) => {
-              set((state) => ({ ...state, isLoadingCrime: false }));
-              toast.error(`${error.message}`, {
-                position: 'bottom-center',
-              });
-            });
-        },
+        ...StoreActions,
       }),
       {
         name: 'app-storage',
