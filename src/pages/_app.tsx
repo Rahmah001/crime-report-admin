45f67f7deb55417559508ffc02a2e5492ecae05e
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-06 18:03:38 +0100

    chore: moved Toast Component inside Chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,8 +14,8 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <ClientOnly>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
+        <Toaster />
       </ChakraProvider>
-      <Toaster />
     </ClientOnly>
   );
 };
